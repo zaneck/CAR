83a475c7409658aba31f2e1e3d8955e39ceeff8f
zaneck <val.owczarek@gmail.com>
2015-01-20 20:59:07 +0100

    modification signature

---
+++ src/ftp/FtpRequest.java
@@ -0,0 +1,44 @@
+package ftp;
+
+public class FtpRequest {
+
+	public void processRequest(String request){
+		
+	}
+	
+	public void processUSER(){
+		
+	}
+	
+	public void processPASS(){
+
+	}
+
+	public void processRETR(){
+
+	}
+
+	public void processSTOR(){
+
+	}
+
+	public void processLIST(){
+
+	}
+
+	public void processQUIT(){
+
+	}
+
+	public void processPWD(){
+
+	}
+	
+	public void processCWD(String directory){
+		
+	}
+	
+	public void CDUP(){
+		
+	}
+}
