5e0ddb035d0ae496a8cea5eb9da1e4eb04dad0db
zaneck <val.owczarek@gmail.com>
2015-01-27 17:30:24 +0100

    fin ecriture serveur

--- src/ftp/Serveur.java
+++ src/ftp/Serveur.java
@@ -1,10 +1,39 @@
 package ftp;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Serveur {
-
+	
 	public static void main(String[] args) {
+		 if(args.length !=1){
+			 System.err.println("Port non specifié");
+		 }
+		 		 
+		 /*creation serversocket*/
+		 try {
+			@SuppressWarnings("resource")
+			final ServerSocket ss = new  ServerSocket(Integer.parseInt(args[0]));
 		
-
+		/*fin creation serversocket*/
+		 
+		Socket socket =null;
+		
+		/*accepter une connection passer la main a un thread*/
+		while(true){
+			socket = ss.accept();
+			Thread tFtp =new Thread(new FtpRequest(socket));
+			tFtp.start();
+		}
+		
+		 } catch (NumberFormatException e) {
+			 System.err.println("Argument erreur");
+			 e.printStackTrace();
+		 } catch (IOException e) {
+			 System.err.println("creation serveur erreur");
+			 e.printStackTrace();
+		 }
 	}
 
 }
