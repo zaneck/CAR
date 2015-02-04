85b41f863e2d627f00f3c10792be94508b29d319
zaneck <val.owczarek@gmail.com>
2015-02-04 10:46:15 +0100

    travail en tp

--- src/ftp/FtpRequest.java
+++ src/ftp/FtpRequest.java
@@ -2,19 +2,28 @@ package ftp;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.util.ArrayList;
+
+import sun.nio.ch.SocketAdaptor;
 
 public class FtpRequest implements Runnable{
 
+	private static final String ERREUR_WRITE_BW = "erreur write";
+	
 	private Socket socket;
 	private String commandeCourante;
 	private BufferedWriter bw;
 	private BufferedReader br;
+	private String user;
+	private BufferedWriter bwData;
 
 	public FtpRequest(Socket socket){
 		this.socket=socket;
@@ -36,7 +45,7 @@ public class FtpRequest implements Runnable{
 			OutputStreamWriter ouw = new OutputStreamWriter(ou);
 			this.bw = new BufferedWriter(ouw);
 			/*fin BufferedWriter*/
-			char[] ready = "220 ftp server ready\n\r".toCharArray();
+			char[] ready = "----220 ftp server ready----\n\r".toCharArray();
 			bw.write(ready);
 			bw.flush();
 		} catch (IOException e) {
@@ -61,7 +70,6 @@ public class FtpRequest implements Runnable{
 
 	private void parseCommande() {
 		String com = this.commandeCourante.substring(0, 4);
-		System.out.println(com);
 
 		if(com.compareTo("USER")==0){
 			this.processUSER();
@@ -90,51 +98,162 @@ public class FtpRequest implements Runnable{
 		else if(com.compareTo("CDUP")==0){
 			this.processCDUP();
 		}
+		else if(com.compareTo("SYST")==0){
+			this.processSYST();
+		}
+		else if(com.compareTo("PORT")==0){
+			this.processPORT();
+		}
 		else{
 			this.commandeInconnue();
 		}
 	}
 
-	private void commandeInconnue() {
-		// TODO Auto-generated method stub
+	private void processPORT() {
+		System.out.println(this.commandeCourante);
+		String[] co = this.commandeCourante.split(",");
+		int port = Integer.parseInt(co[4])*256+ Integer.parseInt(co[5]);
+		
+		Socket sData;
+		try {
+			sData = new Socket(socket.getInetAddress(), port);
+			this.bwData = new BufferedWriter(new OutputStreamWriter(sData.getOutputStream()));		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			bw.write("200 PORT ok\r\n");
+			bw.flush();
+		} catch (IOException e) {
+			System.err.println(ERREUR_WRITE_BW);
+			e.printStackTrace();
+		}
+	}
 
+	public void processSYST() {
+		try {
+			bw.write("215 UNIX\r\n");
+			bw.flush();
+		} catch (IOException e) {
+			System.err.println(ERREUR_WRITE_BW);
+			e.printStackTrace();
+		}
+		
+	}
+
+	public void commandeInconnue() {
+		System.out.println("comande inconnue "+this.commandeCourante);
+		try {
+			bw.write("502 commande inconnue");
+			bw.flush();
+		} catch (IOException e) {
+			System.err.println(ERREUR_WRITE_BW);
+			e.printStackTrace();
+		}
+		
 	}
 
 	public void processUSER(){
-		System.out.println(this.commandeCourante);
-		System.out.println("To DO!");
+		this.user=commandeCourante.substring(5);
+		System.out.println(this.user);
+		try {
+			bw.write("331 User ok need passphrase\r\n");
+			bw.flush();
+		} catch (IOException e) {
+			System.err.println(ERREUR_WRITE_BW);
+			e.printStackTrace();
+		}
 	}
 
 	public void processPASS(){
-		System.out.println("To DO!");
+		System.out.println("PASS");
+		
+		if(this.user.compareTo("anonymous")==0){
+			System.out.println("anonymous pass");
+			try {
+				bw.write("230 Pass ok\r\n");
+				bw.flush();
+			} catch (IOException e) {
+				System.err.println(ERREUR_WRITE_BW);
+				e.printStackTrace();
+			}
+		}
+		else{
+			if(this.commandeCourante.substring(5).compareTo("hello_world")==0){
+				try {
+					bw.write("230 Pass ok\r\n");
+					bw.flush();
+					System.out.println("login ok");
+				} catch (IOException e) {
+					System.err.println(ERREUR_WRITE_BW);
+					e.printStackTrace();
+				}
+			}
+			else{
+				try {
+					bw.write("530 Pass fail\r\n");
+					bw.flush();
+					System.out.println("login fail");
+				} catch (IOException e) {
+					System.err.println(ERREUR_WRITE_BW);
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
 	public void processRETR(){
-		System.out.println("To DO!");
+		System.out.println("To DO!RETR");
 	}
 
 	public void processSTOR(){
-		System.out.println("To DO!");
+		System.out.println("To DO!STOR");
 	}
 
 	public void processLIST(){
-		System.out.println("To DO!");
+		try {
+			System.out.println("listing");
+			bw.write("150 listing...\n");
+			bw.flush();
+			
+			System.out.println("on liste");
+			
+			bwData.write("bob");
+			bwData.flush();
+			
+			bw.write("226 listing done!");
+			bw.flush();
+			System.out.println("fin listing");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public void processQUIT(){
-		System.out.println("To DO!");
+		System.out.println("QUIT");
+		try {
+			bw.write("426 QUIT\r\n");
+			bw.flush();
+			bw.close();
+			br.close();
+		} catch (IOException e) {
+			System.err.println(ERREUR_WRITE_BW);
+			e.printStackTrace();
+		}
 	}
 
 	public void processPWD(){
-		System.out.println("To DO!");
+		System.out.println("To DO!PWD");
 	}
 
 	public void processCWD(){
-		System.out.println("To DO!");
+		System.out.println("To DO!CWF");
 	}
 
 	public void processCDUP(){
-		System.out.println("To DO!");
+		System.out.println("To DO!CDUP");
 	}
 
 	@Override
