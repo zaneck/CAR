aef44636d130be039eb1f9ad2d47c650219acd1c
zaneck <val.owczarek@gmail.com>
2015-03-10 18:46:17 +0100

    ls completement ok

--- src/rest/resource/FtpResource.java
+++ src/rest/resource/FtpResource.java
@@ -2,7 +2,6 @@ package rest.resource;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import rest.service.FtpService;
@@ -10,6 +9,10 @@ import rest.service.FtpService;
 @Path("/ftp")
 public class FtpResource {
 
+	private static final String LS = "<a href=ls>ls</ls>";
+	private static final String PWD = "<a href=pwd>pwd</ls>";
+	private static final String FTP_SERVER = "<h1>FTP server</h1>";
+	
 	private FtpService client;
 
 	public FtpResource() {
@@ -19,18 +22,25 @@ public class FtpResource {
 	@GET
 	@Produces("text/html")
 	public String sayHello() {
-		return "<h1>Hello World</h1>";
+		return FTP_SERVER +
+				"<ul>"+
+					"<li>"+LS+"</li>"+
+					"<li>"+PWD+"</li>"+
+				"</ul>";
 	}
 
 	@GET
 	@Path("ls")
 	public String ls() {
-		return this.client.ls();		 
+		return this.client.ls();	 
 	}
 
 	@GET
 	@Path("pwd")
 	public String pwd() {
+		this.client.pwd();
+		this.client.pwd();
+		this.client.pwd();
 		return this.client.pwd();
 	}
 }
